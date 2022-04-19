75d53268487290960fc6b38c82512abfd72941d8
Phillip <coin-trader-kr@gmail.com>
2022-04-20 00:52:22 +0900

    fix import

--- contracts/DogGoneBad.sol
+++ contracts/DogGoneBad.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import '@openzeppelin/contracts/utils/Strings.sol';
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts@4.5.0/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 import "erc721psi/contracts/ERC721Psi.sol";
 import "erc721psi/contracts/extension/ERC721PsiBurnable.sol";
